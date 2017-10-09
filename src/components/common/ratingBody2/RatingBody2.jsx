1561479fac2e8bc7306f02acbe59ef6db3abaf94
allenfeng <allenfeng@mintcode.com>
2017-10-09 18:04:25 +0800

    add rating view

--- src/components/common/ratingBody2/RatingBody2.jsx
+++ src/components/common/ratingBody2/RatingBody2.jsx
@@ -21,19 +21,21 @@ class RatingBody2 extends React.Component {
                         <li className="rating2-item border-1px" key={i}>
                             <div className="avatar"><img src={rating.avatar}/></div>
                             <div className="desc">
-                                <div className="info clearfix"><span className="name">{rating.username}</span><span className="time">{moment(rating.rateTime).format('YYYY-MM-DD HH:mm')}</span></div>
+                                <div className="info clearfix"><span className="name">{rating.username}</span><span
+                                    className="time">{moment(rating.rateTime).format('YYYY-MM-DD HH:mm')}</span></div>
                                 <div className="level">
-                                    <Star size={24} score={rating.score} style={{display: 'inline-block'}}/><span
-                                    className="delivery-time">{`${rating.deliveryTime}分钟送达`}</span>
+                                    <Star size={24} score={rating.score}
+                                          style={{display: 'inline-block'}}/>{rating.deliveryTime && <span
+                                    className="delivery-time">{`${rating.deliveryTime}分钟送达`}</span>}
                                 </div>
                                 <p className="text">{rating.text}</p>
                                 <div className="recommend"><i
                                     className={`icon ${rating.rateType === 0 ? "icon-thumb_up" : "icon-thumb_down"}`}/>
-                                        {
-                                            _.map(rating.recommend, (item, i) => (
-                                                <span className="tag" key={i}>{item}</span>
-                                            ))
-                                        }
+                                    {
+                                        _.map(rating.recommend, (item, i) => (
+                                            <span className="tag" key={i}>{item}</span>
+                                        ))
+                                    }
                                 </div>
                             </div>
                         </li>
--- src/components/layouts/seller/Seller.jsx
+++ src/components/layouts/seller/Seller.jsx
@@ -3,14 +3,73 @@
  */
 
 import React from 'react';
+import './seller.less';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import ShopCart from '../goods/ShopCart';
+import * as GoodsActionCreator from '../../../actions/goods'
+import Star from '../../common/star';
+import bScroll from 'better-scroll';
+import ReactBScroll from '../../common/scrollbar';
 
+const options = {
+    click: true
+}
+
+@connect(state => ({
+    seller: state.seller,
+    goods: state.goods,
+    ratings: state.ratings
+}), dispatch => ({
+    goodActions: bindActionCreators(GoodsActionCreator, dispatch)
+}))
 class Seller extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {}
     }
 
     render() {
-        return <div>seller</div>
+        const {info}=this.props.seller;
+        const {goodsInfo, selectFoods}=this.props.goods;
+        const {goodActions} = this.props;
+        return <div className="seller">
+            <ReactBScroll bScroll={bScroll} options={options} onRefresh={(iScrollInstance) => {
+                let hasVerticalScroll = iScrollInstance.y;
+                if (this.state.canVerticallyScroll != hasVerticalScroll) {
+                    this.setState({canVerticallyScroll: hasVerticalScroll})
+                }
+            }}>
+                <div>
+                    <div className="seller-info">
+                        <div className="name">{info.name}</div>
+                        <div className="rating-wrapper border-1px"><Star size={36} score={info.score}
+                                                                         style={{
+                                                                             display: 'inline-block',
+                                                                             verticalAlign: 'top'
+                                                                         }}/><span
+                            className="count">{`(${info.ratingCount})`}</span><span
+                            className="count">{`月售${info.sellCount}单`}</span></div>
+                        <div className="desc">
+                            <div className="lab">
+                                <div className="title">起送价</div>
+                                <div className="text">{info.minPrice}<span className="unit">元</span></div>
+                            </div>
+                            <div className="lab">
+                                <div className="title">商家配送</div>
+                                <div className="text">{info.deliveryPrice}<span className="unit">元</span></div>
+                            </div>
+                            <div className="lab">
+                                <div className="title">平均配送时间</div>
+                                <div className="text">{info.deliveryTime}<span className="unit">分钟</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ReactBScroll>
+            <ShopCart seller={info} selectFoods={selectFoods} addFood={goodActions.addFood}
+                      removeFood={goodActions.removeFood} clearFoods={goodActions.clearFoods}/>
+        </div>
     }
 }
 
