71170d404ed1c04afeb670eba661341c577adde6
allenfeng <allenfeng@mintcode.com>
2017-10-16 17:48:24 +0800

    add react-saga

--- src/actions/goods.js
+++ src/actions/goods.js
@@ -16,6 +16,22 @@ export function getGoods(data) {
     }
 }
 
+/**** change redux-saga
+export function getGoods() {
+    return async(dispatch) => {
+        let res = await fetch('/api/goods', {
+            method: 'get'
+        })
+        let json = await res.json();
+        if (json.errno === resCode.OK) {
+            return dispatch({
+                type: Action.GET_GOODS,
+                data: json.data
+            })
+        }
+    }
+}*/
+
 export function addFood(selectFood) {
     return {
         type: Action.ADD_FOOD,
