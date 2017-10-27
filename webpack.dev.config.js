5046a10c60721b6a9febe820d87ca5843e5c1495
allenfeng <allenfeng@mintcode.com>
2017-10-27 19:11:18 +0800

    fix bug

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -75,6 +75,11 @@ module.exports = {
     },
     devtool: "source-map",
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
